62192f415aba2fd3cb927017b8211fb3bd2f448f
warleyxavier <warley.xavier@stone.com.br>
2021-10-16 20:22:23 -0300

    Feature: created service to transfer amount between two accounts

---
+++ bank-account-ddd-studies/domain/service/TransferService.cs
@@ -0,0 +1,13 @@
+﻿using bank_account_ddd_studies.domain.entity;
+
+namespace bank_account_ddd_studies.domain.service
+{
+    public class TransferService
+    {
+        public void Transfer(Account from, Account to, decimal amount)
+        {
+            from.Debit(amount);
+            to.Credit(amount);
+        }
+    }
+}
