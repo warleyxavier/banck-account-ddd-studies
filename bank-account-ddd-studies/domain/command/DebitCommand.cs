e5bb3f87addd082cadd49f51f7e3b0c220813bd8
warleyxavier <warley.xavier@stone.com.br>
2021-10-17 18:07:57 -0300

    Feature: Created DebitCommand and his andler

---
+++ bank-account-ddd-studies/domain/command/DebitCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace bank_account_ddd_studies.domain.command
+{
+    public class DebitCommand : ICommand
+    {
+        public string AccountId { get; }
+        public decimal Amount { get; set; }
+
+        public string Operation => "Debit";
+
+        public DebitCommand(string accountId, decimal amount)
+        {
+            AccountId = accountId;
+            Amount = amount;
+        }
+    }
+}
---
+++ bank-account-ddd-studies/domain/commandHandler/DebitHandler.cs
@@ -0,0 +1,29 @@
+﻿using bank_account_ddd_studies.domain.command;
+using bank_account_ddd_studies.domain.repository;
+
+namespace bank_account_ddd_studies.domain.commandHandler
+{
+    public class DebitHandler : IHandler
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public string Operation => "Debit";
+
+        public DebitHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public void Execute(ICommand command)
+        {
+            var debitCommand = (DebitCommand)command;
+
+            var account = _accountRepository.Get(debitCommand.AccountId);
+
+            if (account is not null)
+            {
+                account.Debit(debitCommand.Amount);
+            }
+        }
+    }
+}
