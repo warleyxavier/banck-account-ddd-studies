9916c19316b7228f6dc3d6df4e098759d9da31c0
warleyxavier <warley.xavier@stone.com.br>
2021-10-18 17:54:53 -0300

    Feature: Created SearchAccountById query and his handler

---
+++ bank-account-ddd-studies/domain/query/SearchAccountById.cs
@@ -0,0 +1,14 @@
+﻿namespace bank_account_ddd_studies.domain.query
+{
+    public class SearchAccountById : IQuery
+    {
+        public string Id { get; }
+
+        public string Operation => "SearchAccountById";
+
+        public SearchAccountById(string id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ bank-account-ddd-studies/domain/queryHandler/SearchAccountByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using bank_account_ddd_studies.domain.query;
+using bank_account_ddd_studies.domain.repository;
+using System;
+
+namespace bank_account_ddd_studies.domain.queryHandler
+{
+    public class SearchAccountByIdHandler : IQueryHandler
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public string Operation => "SearchAccountById";
+
+        public SearchAccountByIdHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public T Search<T>(IQuery query)
+        {
+            var searchAccount = (SearchAccountById)query;
+
+            var account = _accountRepository.Get(searchAccount.Id) is T;
+
+            return (T)Convert.ChangeType(account, typeof(T));
+        }
+    }
+}
